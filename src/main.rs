b4df538e4737419f599b8d5ac01788c8da869a54
Johan Rouve <johan.rouve@happn.fr>
2019-12-22 09:50:46 +0100

    Refacto + unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use structopt::StructOpt;
-use std::io::{BufReader, BufRead};
 use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+use structopt::StructOpt;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
@@ -17,19 +18,39 @@ fn main() {
     let file_result = File::open(args.path);
 
     let message = match file_result {
-        Ok(file) => print_lines_containing(&args.pattern, file),
+        Ok(file) => get_lines_containing(&args.pattern, &mut BufReader::new(file)),
         Err(error) => format!("File error: {}", error)
     };
 
     println!("{}", message)
 }
 
-fn print_lines_containing(pattern: &String, file: File) -> String {
-    let reader = BufReader::new(file);
+fn get_lines_containing(pattern: &String, reader: &mut dyn BufRead) -> String {
     let matching_strings: Vec<String> = reader.lines()
         .map(Result::unwrap)
         .filter(|line| line.contains(pattern))
         .collect();
 
     matching_strings.join("\n")
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io;
+
+    use super::*;
+
+    #[test]
+    fn empty_file() {
+        let mut reader = io::Cursor::new(b"");
+        let lines_containing = get_lines_containing(&String::from(""), &mut reader);
+        assert_eq!(lines_containing, "");
+    }
+
+    #[test]
+    fn std() {
+        let mut reader = io::Cursor::new(b"line1 coucou\nline2 bibu");
+        let lines_containing = get_lines_containing(&String::from("line1"), &mut reader);
+        assert_eq!(lines_containing, "line1 coucou");
+    }
 }
